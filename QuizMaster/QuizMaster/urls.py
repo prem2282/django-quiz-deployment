a9c9dd620a4f28c6d00353829f28a81cfa12586e
prem <prem2282@gmail.com>
2019-03-15 22:11:10 +0530

    Signed-off-by: prem <prem2282@gmail.com>

--- QuizMaster/QuizMaster/urls.py
+++ QuizMaster/QuizMaster/urls.py
@@ -20,6 +20,7 @@ from quizapi import views
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework_jwt.views import refresh_jwt_token
 from graphene_django.views import GraphQLView
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,5 +29,5 @@ urlpatterns = [
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration', include('rest_auth.registration.urls')),
     path('refresh-token/',refresh_jwt_token),
-    path('graphql/', GraphQLView.as_view(graphiql=True)),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
